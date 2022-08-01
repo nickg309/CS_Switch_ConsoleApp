ba13cff5ff33ea6ad2e1a03de9113f99927c4da3
nicholas gallagher <nickg309@gmail.com>
2022-07-31 17:36:23 -0700

    Complete

--- Program.cs
+++ Program.cs
@@ -6,7 +6,45 @@ namespace CS_Switch_ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input the name of one of these countries and I will output the name of their currecny.");Console.WriteLine("Input the name of one of these countries and I will output the name of their currecny.");
+            Console.WriteLine("United States");
+            Console.WriteLine("Canada");
+            Console.WriteLine("India");
+            Console.WriteLine("Japan");
+            Console.WriteLine("Mexico");
+
+            string answer = Console.ReadLine();
+            CurrencyID(answer);
         }
+
+        private static int CurrencyID(string answer)
+        {
+            switch (answer.ToUpper())
+            {
+                case "UNITED STATES":
+                    Console.WriteLine("The United States uses the US Dollar.");
+                    break;
+
+                case "CANADA":
+                    Console.WriteLine("Canada uses the Canadian Dollar.");
+                    break;
+
+                case "INDIA":
+                    Console.WriteLine("India uses the Indian Rupee.");
+                    break;
+
+                case "JAPAN":
+                    Console.WriteLine("Japan uses the Japanese Yen.");
+                    break;
+
+                case "MEXICO":
+                    Console.WriteLine("Mexico uses the Mecican Peso.");
+                    break;
+            }
+
+            return 0;
+        }
+
+
     }
 }
